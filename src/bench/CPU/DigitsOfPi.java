c4e4f06d5bdc69781faf3cd6f067ad9389810847
aivalfmioc <flaviamioc@yahoo.com>
2021-03-29 17:40:24 +0300

    adaugare

--- src/bench/CPU/DigitsOfPi.java
+++ src/bench/CPU/DigitsOfPi.java
@@ -1,7 +1,9 @@
 package bench.CPU;
-
+import java.util.*;
 public class DigitsOfPi {
-    public static void main(String args[]){
+    public static void main(String [] args){
         System.out.println("3.14");
+        System.out.println("15");
+
     }
 }
