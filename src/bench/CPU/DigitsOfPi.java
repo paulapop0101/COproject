1ce72f0752d4f5cda01eb8d3c3fef5131786f1c1
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-04-12 16:39:03 +0300

    Timer

--- src/bench/CPU/DigitsOfPi.java
+++ src/bench/CPU/DigitsOfPi.java
@@ -1,9 +1,22 @@
 package bench.CPU;
-import java.util.*;
+import timer.Timer;
+import logging.TimeUnit;
+
 public class DigitsOfPi {
     public static void main(String [] args){
+        Timer timer=new Timer();
+        timer.start();
         System.out.println("3.14");
         System.out.println("15");
-
+        long p=timer.pause();
+        System.out.println(p);
+        int t=0;
+        timer.resume();
+        for(int i=0;i<100;i++) t++;
+        long end=timer.stop();
+        System.out.println(end);
+        TimeUnit timeUnit=TimeUnit.Sec;
+        double ti=timeUnit.toTimeUnit(end,timeUnit);
+        System.out.println(ti);
     }
 }
