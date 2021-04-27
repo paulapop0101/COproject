bcea3c15a38881134fd96a3d25022f09bac199da
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-04-27 21:58:19 +0300

    "Primary stage"

--- src/main/java/primary.java
+++ src/main/java/primary.java
@@ -1,1 +1,14 @@
-//primary java
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class primary {
+
+    @FXML
+    private void switchToCPU() throws IOException {
+        Main.setRoot("cpuTest");
+    }
+    @FXML
+    private void switchToRAM() throws IOException {
+        Main.setRoot("ramTest");
+    }
+}
