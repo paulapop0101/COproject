f0d76e4d7c5cc169c19aea61fe3942530e027a3b
Sefora-Iemima-Mezei <sefora.mezei@student.upt.ro>
2021-04-27 22:07:32 +0300

    "RAM test"

--- src/main/java/ramTest.java
+++ src/main/java/ramTest.java
@@ -1,1 +1,10 @@
-//ram test
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ramTest {
+
+    @FXML
+    private void backToPrimary() throws IOException {
+        Main.setRoot("primary");
+    }
+}
