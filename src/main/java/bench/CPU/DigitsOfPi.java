7a076e553ed1786bb52d63b8d34832bc613ee27a
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-05-08 00:12:12 +0300

    "Digits of pi"

--- src/main/java/bench/CPU/DigitsOfPi.java
+++ src/main/java/bench/CPU/DigitsOfPi.java
@@ -7,32 +7,135 @@ import logging.TimeUnit;
 
 import java.io.IOException;
 
-public class DigitsOfPi  {
+import java.util.*;
 
-        private static final int SCALE = 10000;
-    private static final int ARRINIT = 2000;
+public class DigitsOfPi{
 
-    public static void Digits(int digits){
+    private int[] digits;
+    private StringBuilder predigits = new StringBuilder();
 
-        StringBuffer pi = new StringBuffer();
-        int[] arr = new int[digits + 1];
-        int carry = 0;
+    // Max value such that digits.length <= INT_MAX.
+    //   ceil(((2**31-1) - 1) * 3 / 10)
+    private static final int MAX_DIGITS_REQUESTED = 644245094;
 
-        for (int i = 0; i <= digits; ++i)
-            arr[i] = ARRINIT;
 
-        for (int i = digits; i > 0; i-= 14) {
-            int sum = 0;
-            for (int j = i; j > 0; --j) {
-                sum = sum * j + SCALE * arr[j];
-                arr[j] = sum % (j * 2 - 1);
-                sum /= j * 2 - 1;
+    public static void printHelp() {
+        System.err.println("\n  PiSpigot [number of digits requested]\n");
+    }
+
+    // Get the number of digits requested from the command line arguments.
+    public boolean parseArgs(String[] args, int k) {
+        if (args.length != 1) {
+            printHelp();
+            return false;
+        }
+
+        try {
+            k = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            printHelp();
+            return false;
+        }
+
+        if (k <= 0) {
+            System.err.println("Digit count must be positive.");
+            return false;
+        }
+
+        if (k > MAX_DIGITS_REQUESTED) {
+            System.err.println("Maximum digit count is " + MAX_DIGITS_REQUESTED);
+            return false;
+        }
+
+        return true;
+    }
+
+
+    // Allocate digits[]
+    public boolean init(int k) {
+        int array_size_needed = k * 10 / 3 + 1;
+        digits = new int[array_size_needed];
+        if (digits == null) {
+            System.err.printf("Failed to allocate " + (array_size_needed*4)
+                    + " bytes.");
+            return false;
+        }
+
+        // fill each digit with a 2
+        for (int i=0; i < digits.length; i++)
+            digits[i] = 2;
+
+        return true;
+    }
+
+
+    // Produce digits
+    public void Digits(int k) {
+        if (!init(k)) return;
+
+        for (int iter = 0; iter < k; iter++) {
+
+            // Work backwards through the array, multiplying each digit by 10,
+            // carrying the excess and leaving the remainder.
+            int carry = 0;
+            for (int i=digits.length-1; i > 0; i--) {
+                int numerator = i;
+                int denomenator = i * 2 + 1;
+                int tmp = digits[i] * 10 + carry;
+                digits[i] = tmp % denomenator;
+                carry = tmp / denomenator * numerator;
             }
 
-            pi.append(String.format("%04d", carry + sum / SCALE));
-            carry = sum % SCALE;
-        }
-        //System.out.println( pi.toString());
+            // process the last digit
+            int tmp = digits[0] * 10 + carry;
+            digits[0] = tmp % 10;
+            int digit = tmp / 10;
 
+            // implement buffering and overflow
+            if (digit < 9) {
+                flushDigits();
+                // print a decimal after the leading "3"
+                if (iter == 1) System.out.print(".");
+                addDigit(digit);
+            } else if (digit == 9) {
+                addDigit(digit);
+            } else {
+                overflowDigits();
+                flushDigits();
+                addDigit(0);
+            }
+            // System.out.flush();
+        }
+        flushDigits();
+        System.out.println();
+    }
+
+
+    // write the buffered digits
+    void flushDigits() {
+        System.out.append(predigits);
+        predigits.setLength(0);
+    }
+
+
+    // given an integer 0..9, buffer a digit '0' .. '9'
+    void addDigit(int digit) {
+        predigits.append((char)('0' + digit));
+    }
+
+
+    // add one to each digit, rolling over from from 9 to 0
+    void overflowDigits() {
+        for (int i=0; i < predigits.length(); i++) {
+            char digit = predigits.charAt(i);
+            // This could be implemented with a modulo, but compared to the main
+            // loop this code is too quick to measure.
+            if (digit == '9') {
+                predigits.setCharAt(i, '0');
+            } else {
+                predigits.setCharAt(i, (char)(digit + 1));
+            }
+        }
     }
 }
+
--- src/main/java/cpuTest.java
+++ src/main/java/cpuTest.java
@@ -15,7 +15,7 @@ public class cpuTest {
     @FXML
     private Label text;
     @FXML
-    public void initialize() { value.getItems().addAll(16000, 32000,64000,128000,256000,512000,1000000,2000000,4000000,8000000,16000000,32000000);
+    public void initialize() { value.getItems().addAll(16000, 32000,64000,128000,25600,512000,10000,2000000,4000000,8000000,16000000,32000000);
     }
     @FXML
     private void backToPrimary() throws IOException {
