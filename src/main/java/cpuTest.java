ac9c9954e0b0908bce9e9a1b7b531080541bc26e
Paula <poppaula0106@gmail.com>
2021-05-07 23:16:33 +0300

    "cpu interface added"

--- src/main/java/cpuTest.java
+++ src/main/java/cpuTest.java
@@ -4,11 +4,16 @@ import bench.CPU.DigitsOfPi;
 import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import timer.Timer;
 
 public class cpuTest {
 
     @FXML
     private ChoiceBox value;
+    @FXML
+    private Label text;
     @FXML
     public void initialize() { value.getItems().addAll(16000, 32000,64000,128000,256000,512000,1000000,2000000,4000000,8000000,16000000,32000000);
     }
@@ -20,7 +25,10 @@ public class cpuTest {
     private void test() throws IOException {
         DigitsOfPi d = new DigitsOfPi();
         int k=Integer.parseInt(String.valueOf(value.getValue()));
+        Timer t = new Timer();
+        t.start();
         d.Digits(k);
+        text.setText(""+t.stop());
 
     }
 }
